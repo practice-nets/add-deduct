967f515d008a8598004dbe82df7cffb5213d78aa
Thisara Rathnapala <thisaratr94@gmail.com>
2023-07-22 11:45:29 +0530

    Add two numbers program

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Ex1{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		int num1=input.nextInt();
+		int num2=input.nextInt();
+		System.out.println(num1+num2);
+	}
+}
